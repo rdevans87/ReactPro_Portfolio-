49284baac7c00951e9bbaa62d8d0f0f3d44df263
Ryan Evans <rdevans87@gmail.com>
2021-06-04 19:00:30 -0400

    add a card and css to work section

--- ryansreactapp/src/components/Work.js
+++ ryansreactapp/src/components/Work.js
@@ -5,19 +5,16 @@ import "../styles/Work.css";
 function Work() {
   return (
     <section className="work">
-      <h2>Work</h2>
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-        dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-        consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-        molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-      </p>
+        <h2>Work</h2>
+     <div class="container">
+     <div class="card">
+       <div class="face face1">
+         <div class="content"></div>
+      <h2>Project</h2>
+      <a href="#" type="button">Read More</a>
+         </div>
+       </div>
+    </div>
     </section>
   );
 }

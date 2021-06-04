44e838c0f0211de203f2e187f5f38b2d88743aa2
Ryan Evans <rdevans87@gmail.com>
2021-06-04 19:19:19 -0400

    adding cards  styling and headers for projects

--- ryansreactapp/src/components/Work.js
+++ ryansreactapp/src/components/Work.js
@@ -6,20 +6,53 @@ function Work() {
   return (
     <section className="work">
         <h2>Work</h2>
-     <div class="container">
+        <div class="container">
      <div class="card">
        <div class="face face1">
-         <div class="content"></div>
-      <h2>Project 1</h2>
-      </div>
-      </div>
-      <div class="face face2">
-      <div class="content">
-      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
-      <a href="#" type="button">Read More</a>
+         <div class="content">
+            <i class="fab fa-windows"></i>            
+           <h3>Project 1</h3>
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           <a href="#" type="button">Read More</a>
          </div>
        </div>
     </div>
+    
+    <div class="card">
+       <div class="face face1">
+         <div class="content">
+            <h3>Project 2</h3>
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           <a href="#" type="button">Read More</a>
+         </div>
+       </div>
+    </div>
+    
+    
+    <div class="card">
+       <div class="face face1">
+         <div class="content">
+            <h3>Project 3</h3>
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           <a href="#" type="button">Read More</a>
+         </div>
+       </div>
+    </div>
+    
+  </div>
+    
     </section>
   );
 }

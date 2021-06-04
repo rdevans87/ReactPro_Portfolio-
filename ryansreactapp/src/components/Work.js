2dd3711d45357136b6dc152f323d03ea3a0ed8c8
Ryan Evans <rdevans87@gmail.com>
2021-06-04 19:11:12 -0400

    working on cards to for works section

--- ryansreactapp/src/components/Work.js
+++ ryansreactapp/src/components/Work.js
@@ -10,7 +10,12 @@ function Work() {
      <div class="card">
        <div class="face face1">
          <div class="content"></div>
-      <h2>Project</h2>
+      <h2>Project 1</h2>
+      </div>
+      </div>
+      <div class="face face2">
+      <div class="content">
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
       <a href="#" type="button">Read More</a>
          </div>
        </div>

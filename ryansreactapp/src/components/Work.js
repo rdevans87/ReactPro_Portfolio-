819f94fa472d23aa3f46335b773109d7b0ff53dc
Ryan Evans <rdevans87@gmail.com>
2021-06-05 11:14:24 -0400

    working on cards for portfolio

--- ryansreactapp/src/components/Work.js
+++ ryansreactapp/src/components/Work.js
@@ -6,45 +6,57 @@ function Work() {
   return (
     <section className="work">
         <h1>Work</h1>
-        <div class="container">
-     <div class="card">
-       <div class="face face1">
-         <div class="content">
-            <i class="fab fa-windows"></i>            
-           <h3>Project 1</h3>
+        <div className="container">
+     <div className="card">
+       <div className="face face1">
+         <div className="content">         
+           <h3>PetTrax</h3>
          </div>
        </div>
-       <div class="face face2">
-         <div class="content">
+       <div className="face face2">
+         <div className="content">
            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
            <a href="#" type="button">Read More</a>
          </div>
        </div>
     </div>
     
-    <div class="card">
-       <div class="face face1">
-         <div class="content">
-            <h3>Project 2</h3>
+    <div className="card">
+       <div className="face face1">
+         <div className="content">
+            <h3>Employee Tracker</h3>
          </div>
        </div>
-       <div class="face face2">
-         <div class="content">
+       <div className="face face2">
+         <div className="content">
            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
            <a href="#" type="button">Read More</a>
          </div>
        </div>
     </div>
     
-    
-    <div class="card">
-       <div class="face face1">
-         <div class="content">
+    <div className="card">
+       <div className="face face1">
+         <div className="content">
             <h3>Project 3</h3>
          </div>
        </div>
-       <div class="face face2">
-         <div class="content">
+       <div className="face face2">
+         <div className="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           <a href="#" type="button">Read More</a>
+         </div>
+       </div>
+    </div>
+
+    <div className="card">
+       <div className="face face1">
+         <div className="content">
+            <h3>Project 3</h3>
+         </div>
+       </div>
+       <div className="face face2">
+         <div className="content">
            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
            <a href="#" type="button">Read More</a>
          </div>

1d1a21c803dc0cc23f5f3a38f468298c8eb542df
Ryan Evans <rdevans87@gmail.com>
2021-06-05 21:52:34 -0400

    fixing up css hover

--- ryansreactapp/src/components/Footer.js
+++ ryansreactapp/src/components/Footer.js
@@ -5,11 +5,14 @@ import "../styles/Footer.css";
 function Footer() {
     return (
       <footer className="footer">
-        
-        <div><h1>Ryan Evans</h1></div>
-        
-      </footer>
-    );
+        <a href="/">Facebook</a>
+        <a href="/">Linkedin</a>
+        <a href="/">Email</a>
+        <a href="/">Phone</a>
+
+    </footer>
+    
+);
   }
 
   export default Footer;

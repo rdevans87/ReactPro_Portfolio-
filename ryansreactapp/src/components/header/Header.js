81ba977daf99bf66b3a70b1e4a5f73ffafbe3333
Ryan Evans <rdevans87@gmail.com>
2021-06-07 04:07:57 -0400

    updates to header js

--- ryansreactapp/src/components/header/Header.js
+++ ryansreactapp/src/components/header/Header.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { Route, Redirect, HashRouter } from "react-router-dom";
 import Navbar from "../../components/Navbar";
-import Navbar from "../../components/About";
-import Navbar from "../../components/Work";
-import Navbar from "../../components/Resume";
-import Navbar from "../../components/Contact";
+import About from "../../components/About";
+import Work from "../../components/Work";
+import Resume from "../../components/Resume";
+import Contact from "../../components/Contact";
 import "../styles/Header.css";
 
 // By importing the Header.css file, it is added to the DOM whenever this component loads
@@ -18,10 +18,11 @@ class Header extends Component {
         </header>
         <div className="conent">
           <Route exact path="/" render={() => (<Redirect to="/about" />)} />
-          <Route path="/work" component={Work} />
-          <Route path="/resume" component={Resume} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/about" component={About} />
+          <Route path="/Navbar" component={Navbar}/>
+          <Route path="/work" component={Work}/>
+          <Route path="/resume" component={Resume}/>
+          <Route path="/contact" component={Contact}/>
+          <Route path="/about" component={About}/>
         </div>
       </Hashrouter>
     )

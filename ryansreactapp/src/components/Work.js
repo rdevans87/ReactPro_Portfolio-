abe7eed8722517723c73c1b9c5c04a8012d578a2
Ryan Evans <rdevans87@gmail.com>
2021-06-06 23:44:19 -0400

    added new cards to page

--- ryansreactapp/src/components/Work.js
+++ ryansreactapp/src/components/Work.js
@@ -35,24 +35,7 @@ function Work() {
        </div>
        </div>
   
-    
-  
     <div className="card card3">
-       <div className="face face1">
-         <div className="content">
-            <h3>Project 3</h3>
-         </div>
-       </div>
-       <div className="face face2">
-         <div className="content">
-           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
-           <a href="/" type="button">Read More</a>
-         </div>
-       </div>
-       </div>
-  
-
-    {/* <div className="card">
        <div className="face face1">
          <div className="content">
             <h3>Weather Dashboard</h3>
@@ -64,7 +47,49 @@ function Work() {
            <a href="/" type="button">Read More</a>
          </div>
        </div>
-    </div> */}
+       </div>
+
+       <div className="card card4">
+       <div className="face face1">
+         <div className="content">
+            <h3>E-Commerce Backend</h3>
+         </div>
+       </div>
+       <div className="face face2">
+         <div className="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           <a href="/" type="button">Read More</a>
+         </div>
+       </div>
+       </div>
+
+       <div className="card card5">
+       <div className="face face1">
+         <div className="content">
+            <h3>NoteTaker App</h3>
+         </div>
+       </div>
+       <div className="face face2">
+         <div className="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           <a href="/" type="button">Read More</a>
+         </div>
+       </div>
+       </div>
+
+       <div className="card card6">
+       <div className="face face1">
+         <div className="content">
+            <h3>ReadMe Generator</h3>
+         </div>
+       </div>
+       <div className="face face2">
+         <div className="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           <a href="/" type="button">Read More</a>
+         </div>
+       </div>
+       </div>
 
   </div>
 

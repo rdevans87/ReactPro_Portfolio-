3625d7b65e6d6601df211563d819854e7472f8c4
Ryan Evans <rdevans87@gmail.com>
2021-06-07 04:05:09 -0400

    file changes to react router syntax

--- ryansreactapp/src/components/header/Header.js
+++ ryansreactapp/src/components/header/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Route, Redirect, HashRouter} from "react-router-dom";
+import { Route, Redirect, HashRouter } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import Navbar from "../../components/About";
 import Navbar from "../../components/Work";
@@ -9,19 +9,28 @@ import "../styles/Header.css";
 
 // By importing the Header.css file, it is added to the DOM whenever this component loads
 
-function Header extends Component () {
+class Header extends Component {
   render() {
-  return (
-    <Hashrouter>
-    <header className="header">
-      
-      <div><h1>Ryan Evans</h1></div>
-      
-    </header>
-    </Hashrouter>
-  )
+    return (
+      <Hashrouter>
+        <header className="header">
+          <div><h1>Ryan Evans</h1></div>
+        </header>
+        <div className="conent">
+          <Route exact path="/" render={() => (<Redirect to="/about" />)} />
+          <Route path="/work" component={Work} />
+          <Route path="/resume" component={Resume} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/about" component={About} />
+        </div>
+      </Hashrouter>
+    )
+
+  }
 }
-}
+
+
+
 
 
 export default Header;

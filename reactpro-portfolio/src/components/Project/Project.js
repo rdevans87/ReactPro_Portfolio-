7580d981aa13d808d238fc7d635619f15cc0ec3d
Ryan Evans <rdevans87@gmail.com>
2021-06-10 15:48:27 -0400

    unable to get images to render on cards

--- reactpro-portfolio/src/components/Project/Project.js
+++ reactpro-portfolio/src/components/Project/Project.js
@@ -12,14 +12,13 @@ function RepoCards(props) {
      <div className="card">
        <div className="face face1">
          <div className="content">  
-         <img alt={props.name} src={props.image}/>       
+         <img src={props.image} alt={props.name}/>       
            <h3>{props.name}</h3>
          </div>
        </div>
        <div className="face face2">
          <div className="content">
            <p>{props.name}</p>  
-           <p>{props.tools}</p>
            <br></br>
            <p>{props.tools}</p>
            <a href={props.github}><img src="https://img.icons8.com/fluent/48/4a90e2/github.png" width="30px" alt="Repository" id="icon"/></a> 

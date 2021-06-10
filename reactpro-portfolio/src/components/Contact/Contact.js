6d5a68e9728a08ae0cf861ce02fbf57aed2ea3c5
Ryan Evans <rdevans87@gmail.com>
2021-06-10 13:31:08 -0400

    adding button to submit form

--- reactpro-portfolio/src/components/Contact/Contact.js
+++ reactpro-portfolio/src/components/Contact/Contact.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./Contact.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+function handleSubmit(e) {
+  e.preventDefault();
+}
+
 // By importing the Contact.css file, it is added to the DOM whenever this component loads
 function Contact() {
   return (
@@ -13,15 +17,15 @@ function Contact() {
       <input className="form-control" type="text" name="name"></input>
       </div>
       <div>
-      <label htmlFor="name" placeholder="full name"></label>
+      <label htmlFor="email" placeholder="email"></label>
       <input className="form-control" type="text" name="name"></input>
       </div>
       <div>
-      <label htmlFor="name" placeholder="full name"></label>
+      <label htmlFor="message" placeholder="message"></label>
       <textarea className="form-control" type="text" name="name"></textarea>
       </div>
       <div>
-      <button></button>
+      <button className="btn btn-primary my-4" type="submit" onSubmit={handleSubmit}>Submit</button>
       </div>
       </form>
   

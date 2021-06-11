c4558558ab6d3c4e2bf45d2c79ef36f1dd7b23db
Ryan Evans <rdevans87@gmail.com>
2021-06-11 10:09:49 -0400

    upload new icons

--- reactpro-portfolio/src/components/Resume/Resume.js
+++ reactpro-portfolio/src/components/Resume/Resume.js
@@ -10,7 +10,9 @@ function Resume() {
       <div>
       <img src="" width="300px" alt="profile" className="profile"></img> 
       <h3>Skills:</h3>
-      <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/bootstrap.png" width="300px" alt="html5" className="profile"></img> 
+      <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/bootstrap.png" width="60px" alt="bootstrap" className="profile"></img> 
+      <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/bootstrap.png" width="60px" alt="bootstrap" className="profile"></img> 
+      <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/bootstrap.png" width="60px" alt="bootstrap" className="profile"></img> 
        <h3>Certificates:</h3>
        <ul>
          <li>Full Stack Coding Bootcamp (2021)</li>
@@ -21,21 +23,21 @@ function Resume() {
       <br></br>
        <h3>Employment:</h3>
        <br></br>
-       Customer Service Specialist @ <img src="" alt="imdb/>"
+       Customer Service Specialist @ <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/icons8-imdb.png" width="60px" alt="imdb"/>
        <br></br>
-       Sr. Technical Support Analyst @
+       Sr. Technical Support Analyst @ <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/castncrew.png" width="80px" alt="castandcrew"/>
       <br></br>
-     
+      Sales Support Associate @
       <br></br>
       <h3>Education:</h3>
       <br></br>
       B.A. Communication and Media Studies.
       </div>
       <div>
-        <p>View or Download my full <a href="http://localhost:52330/reactpro-portfolio/src/assets/PorfolioResume_RyanEvans.pdf" width="20px" rel="noreferrer" target="_blank" >Resume.</a></p>
+        <p>View or Download my full <a href="http://localhost:52330/reactpro-portfolio/src/assets/PorfolioResume_RyanEvans.pdf" width="30px" rel="noreferrer" target="_blank" >Resume.</a></p>
         
       </div>
-    </section>
+      </section>
 
 
   );

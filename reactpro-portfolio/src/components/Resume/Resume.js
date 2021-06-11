1840efc2eba22621caf21112dde08654040b200d
Ryan Evans <rdevans87@gmail.com>
2021-06-11 13:22:08 -0400

    updates to resumre

--- reactpro-portfolio/src/components/Resume/Resume.js
+++ reactpro-portfolio/src/components/Resume/Resume.js
@@ -6,23 +6,24 @@ function Resume() {
   return (
 
     <section className="resume">
-      <h2>Resume</h2>
+      <h1>Resume</h1>
       <div>
-      <img src="" width="300px" alt="profile" ></img> 
+      <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/profilepic.jpg" width="300px" alt="profile" ></img> 
       <h3>Skills:</h3>
       <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/html5.png" width="60px" alt="HTML5" label="html5"></img> 
       <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/css3.png" width="60px" alt="CSS3" ></img> 
       <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/javascript.png" width="60px" alt="JS ES6" ></img>
       <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/icons8-react.png" width="50px" alt="React" ></img> 
-      <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/nodejs.png" width="60px" alt="NodeJS" ></img> 
+      <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/nodejs.png" width="70px" alt="NodeJS" ></img> 
       <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/bootstrap.png" width="60px" alt="Boostrap" ></img> 
-      <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/mysql_logo.png" width="60px" alt="MySQl" ></img> 
+      <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/mysql_logo.png" width="70px" alt="MySQl" ></img> 
       <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/mongodb.png" width="60px" alt="MonogoDB" ></img> 
       <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/icons8-visual_studio_code_2019.png" width="60px" alt="VSCode" ></img> 
       <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/git.png" width="60px" alt="Git" ></img> 
+      <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/icons8-rest_api.png" width="60px" alt="API" ></img> 
+      <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/npm.png" width="60px" alt="NPM" ></img> 
       <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/json.png" width="60px" alt="JSON" ></img> 
-      <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/bootstrap.png" width="60px" alt="Bootstrap" ></img> 
-      <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/bootstrap.png" width="60px" alt="Bootstrap" ></img> 
+      
       <br></br>
        <h3>Certificates:</h3>
        <ul>
@@ -34,13 +35,13 @@ function Resume() {
       <br></br>
        <h3>Employment:</h3>
        <br></br>
-       Customer Service Specialist @ <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/icons8-imdb.png"width="60px" alt="imdb"/>
+       Customer Service Specialist @ <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/icons8-imdb.png"width="90px" alt="imdb"/>
        <br></br>
        <br></br>
-       Sr. Technical Support Analyst @ <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/castncrew.png" width="70px" alt="castandcrew"/>
+       Sr. Technical Support Analyst @ <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/castncrew.png" width="95px" alt="castandcrew"/>
       <br></br>
       <br></br>
-      Sales Support Associate @ <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/finaldraft.png" width="80px" height="30px" alt="finaldraft"/>
+      Sales Support Associate @ <img src="https://raw.githubusercontent.com/rdevans87/ReactPro_Portfolio/main/reactpro-portfolio/src/assets/icons/finaldraft.png" width="100px" height="40px" alt="finaldraft"/>
       <br></br>
       <br></br>
       <h3>Education:</h3>

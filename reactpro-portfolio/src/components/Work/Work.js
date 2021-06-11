68ff4231dfd24da4146671e9107df15be7ef57ed
Ryan Evans <rdevans87@gmail.com>
2021-06-11 06:51:29 -0400

    working on mobile first approach

--- reactpro-portfolio/src/components/Work/Work.js
+++ reactpro-portfolio/src/components/Work/Work.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from "react-router";
 import RepoCards from "../Project/Project";
 import work from "../../projects.json";
 import "../Work/Work.css"
 // import "./Project/Project";
 
-// function Wrapper(props) {
-//   return <div className="wrapper">{props.children}</div>;
+function Wrapper(props) {
+  return <div className="wrapper">{props.children}</div>;
 
-// }
+}
 
 // By importing the Work.css file, it is added to the DOM whenever this component loads
 function Work() {
@@ -15,15 +15,13 @@ function Work() {
   return (
     <section className="work">
       <h1>Work</h1>
-
-      {/* <Wrapper> */}
+   
+      <Wrapper id="project-data">
+    
         {work.map((project => (
           <RepoCards key={project.id} image={project.image} name={project.name}
-            github={project.github} deploy={project.deploy} summary={project.summary} tools={project.tools} />)))};
-
-            {/* </Wrapper> */}
-
-
+            github={project.github} deploy={project.deploy} summary={project.summary} tools={project.tools} />)))};           
+        </Wrapper>
     </section>
 
 
